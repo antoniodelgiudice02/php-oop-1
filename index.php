e48919f5ed397c8fb7b1ffc98ecddf4f0c3adb0b
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-03-04 16:31:45 +0100

    add object

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
 
+class Production
+{
+    public $title;
+    public $language;
+    public $vote;
 
+    public function __construct($title, $language, $vote){
+        $this->title = $title;
+        $this->language = $language;
+        $this->vote = $vote;
+    }
+}
+
+$goodfellas = new Production('Good Fellas', 'En', '10');
+
+var_dump( $goodfellas );
 
 ?>
 
@@ -12,6 +27,8 @@
     <title>Document</title>
 </head>
 <body>
+
+    <h1></h1>
     
 </body>
 </html>
