0cbefb20e3dcaa05aed87e7f0e3ccb6e7126ddd8
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-03-04 17:07:26 +0100

    add echo in table

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 
 class Production
 {
-    public $title;
-    public $language;
-    public $vote;
+    public string $title;
+    public string $language;
+    public int $vote;
 
-    public function __construct($title, $language, $vote){
+    public function __construct($title, $language, $vote)
+    {
         $this->title = $title;
         $this->language = $language;
         $this->vote = $vote;
@@ -14,21 +15,49 @@ class Production
 }
 
 $goodfellas = new Production('Good Fellas', 'En', '10');
+$casino = new Production('Casino', 'En', '10');
 
-var_dump( $goodfellas );
+$productions = [
+    $goodfellas,
+    $casino,
+];
+
+var_dump($productions);
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
 
-    <h1></h1>
-    
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Titolo</th>
+                <th scope="col">Lingua</th>
+                <th scope="col">Voto</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($productions as $production ):?>
+                <tr >
+                    <td><?= $production->title ?></td>
+                    <td><?= $production->language ?></td>
+                    <td><?= $production->vote ?></td>
+                </tr >
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
 </body>
+
 </html>
